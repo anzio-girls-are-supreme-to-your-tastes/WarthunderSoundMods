42c96877214fcd444612dcc649703659157df111
anzio-girls-are-supreme-to-your-tastes <73777440+anzio-girls-are-supreme-to-your-tastes@users.noreply.github.com>
2020-12-02 00:24:05 -0600

    Add files via upload

--- WindowsScripts/azur_raw_bgm_convert.bat
+++ WindowsScripts/azur_raw_bgm_convert.bat
@@ -2,37 +2,11 @@
 echo "VGMToolbox must be used to convert B archives to HCA archives. Contine to populate archives"
 pause
 mkdir CONVERT
-FOR /R . %%i IN (*.hca) DO MOVE /y "%%i" ".\CONVERT\"
+FOR /R . %%i IN (*.hca) DO MOVE /y "%%i" ".\CONVERT\" >nul
 echo "CONVERT directory populated with HCA archives. Continue to Clean up CONVERT directory"
 pause
 cd CONVERT
-del /f warcry*.hca
-del /f upgrade*.hca
-del /f unlock*.hca
-del /f ui-*.hca
-del /f touch*.hca
-del /f task*.hca
-del /f skill*.hca
-del /f shop*.hca
-del /f profile*.hca
-del /f present*.hca
-del /f mvp*.hca
-del /f main*.hca
-del /f mission*.hca
-del /f mail*.hca
-del /f lose_*.hca
-del /f login_*.hca
-del /f link*.hca
-del /f kizuna*.hca
-del /f hp*.hca
-del /f home*.hca
-del /f get*.hca
-del /f feeling*.hca
-del /f extra*.hca
-del /f enter*.hca
-del /f detail*.hca
-del /f expedition*.hca
-del /f buy*.hca
+del /f warcry*.hca upgrade*.hca unlock*.hca ui-*.hca touch*.hca task*.hca skill*.hca shop*.hca profile*.hca present*.hca mvp*.hca main*.hca mission*.hca mail*.hca lose_*.hca login_*.hca link*.hca kizuna*.hca hp*.hca home*.hca get*.hca feeling*.hca extra*.hca enter*.hca detail*.hca expedition*.hca buy*.hca logo.hca lose.hca presnet_like.hca propose.hca purchase.hca build_ai.hca hololivevoice.hca 
 echo "Directory Cleaned. Continue to convert HCA archives to WAV"
 pause
 vgaudiocli.exe -b --out-format wav -i . -o .
